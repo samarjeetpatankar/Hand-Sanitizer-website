fd895e436f77116d4fc10cfbb0afc17fa95bf787
Aniket <anidubey7896@gmail.com>
2023-05-18 12:20:25 +0530

    cart

--- cart.js
+++ cart.js
@@ -1,16 +1,16 @@
-// import navbar from
+//import navbar from
 
-// let cartArr=JSON.parse(localStorage.getItem(""))||[];
+let cartArr=JSON.parse(localStorage.getItem(""))||[];
 
-// if(cartArr.length>0){
-//     displayCart();
-//     calculatePrice();
-// }else{
-//     document.getElementById("parent").innerHTML="Your Cart is currently empty";
-// }
+if(cartArr.length>0){
+    displayCart();
+    calculatePrice();
+}else{
+    document.getElementById("parent").innerHTML="Your Cart is currently empty";
+}
 
-// const displayCart=()=>{
-//     cartArr.forEach(element => {
-        
-//     });
-// }
+const displayCart=()=>{
+    cartArr.forEach(element => {
+        let div=docu
+    });
+}
